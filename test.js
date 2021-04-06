1e170906f7e1fcbda0bc6eb70052fcee27b3f60e
raksa <eng.raksa@gmail.com>
2021-04-06 12:52:54 -0500

    update

--- test.js
+++ test.js
@@ -12,19 +12,48 @@ const _walk = (wallMap, n, stepped, x, y) => {
     return _walk(wallMap, n, Object.assign({}, stepped), x, y + 1) || _walk(wallMap, n, Object.assign({}, stepped), x + 1, y) ||
         _walk(wallMap, n, Object.assign({}, stepped), x, y - 1) || _walk(wallMap, n, Object.assign({}, stepped), x - 1, y);
 }
-function pathFinder(maze) {
-    let pathSteps = maze.split('\n').map(e => e.split(''));
-    const N = pathSteps.length;
-    const n = N - 1;
-    const wallMap = {};
+// function pathFinder(maze) {
+//     let pathSteps = maze.split('\n').map(e => e.split(''));
+//     const N = pathSteps.length;
+//     const n = N - 1;
+//     const wallMap = {};
+//     let upperWall = 0;
+//     let lowerWall = 0;
+//     pathSteps.forEach((e, i) => {
+//         e.forEach((e1, i1) => {
+//             if (e1 === 'W') {
+//                 if (i + i1 > N) {
+//                     upperWall++;
+//                 } else if (i + i1 < N) {
+//                     lowerWall++;
+//                 }
+//             }
+//         });
+//     });
+//     if (upperWall > lowerWall) {
+//         pathSteps = pathSteps.map(e => {
+//             return e.reverse();
+//         }).reverse();
+//     }
+//     pathSteps.forEach((e, i) => {
+//         e.forEach((e1, i1) => {
+//             if (e1 === 'W') {
+//                 wallMap[`${i},${i1}`] = true;
+//             }
+//         });
+//     });
+//     return _walk(wallMap, n, { '0,0': true }, 1, 0) || _walk(wallMap, n, { '0,0': true }, 0, 1)
+// }
+function shouldReverse(pathSteps) {
+    const l = pathSteps.length;
     let upperWall = 0;
     let lowerWall = 0;
     pathSteps.forEach((e, i) => {
         e.forEach((e1, i1) => {
             if (e1 === 'W') {
-                if (i + i1 > N) {
+                if (i + i1 > l) {
                     upperWall++;
-                } else if (i + i1 < N) {
+                } else if (i + i1 < l) {
                     lowerWall++;
                 }
             }
@@ -35,13 +64,18 @@ function pathFinder(maze) {
             return e.reverse();
         }).reverse();
     }
-    pathSteps.forEach((e, i) => {
-        e.forEach((e1, i1) => {
-            if (e1 === 'W') {
-                wallMap[`${i},${i1}`] = true;
-            }
-        });
-    });
+    return pathSteps;
+}
+function pathFinder(maze) {
+    let pathSteps = maze.split('\n').map(e => e.split(''));
+    const n = pathSteps.length - 1;
+    pathSteps = shouldReverse(pathSteps);
+    const wallMap = pathSteps.reduce((s, e, i) => {
+        return e.reduce((s1, e1, i1) => {
+            if (e1 === 'W') s1[`${i},${i1}`] = true;
+            return s;
+        }, s);
+    }, {});
     return _walk(wallMap, n, { '0,0': true }, 1, 0) || _walk(wallMap, n, { '0,0': true }, 0, 1)
 }
 /*
