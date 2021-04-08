f02dac29a8bf32fb80c75b39ee2baf370078aa85
raksa <eng.raksa@gmail.com>
2021-04-08 09:42:13 -0500

    update

--- test1.js
+++ test1.js
@@ -1,23 +1,30 @@
-class A {
-    static name() {
-        throw new Error('name must be implemented');
+const Calculator = function () {
+    this.evaluate = function (string) {
+        string = string.replace(/\s/g, '');
+        let r = null;
+        if (/\+/.test(string)) {
+            r = string.split('+').reduce((s, e) => s + this.evaluate(e), 0);
+        } else if (/\-/.test(string)) {
+            const arr = string.split('-');
+            const first = (string[0] == '-' ? -1 : 1) * this.evaluate(arr.shift());
+            r = arr.reduce((s, e) => s - this.evaluate(e), first);
+        } else if (/\*/.test(string)) {
+            r = string.split('*').reduce((s, e) => s * this.evaluate(e), 1);
+        } else if (/\//.test(string)) {
+            const arr = string.split('/');
+            const first = this.evaluate(arr.shift());
+            r = arr.reduce((s, e) => s / this.evaluate(e), first);
+        } else {
+            r = Number(string);
+        }
+        console.log(`last: ${string} = ${r}`);
+        return r;
     }
-    static toString() {
-        return this.name();
-    }
-    static fromThis(){
-        return new this;
-    }
-}
-class B extends A {
-    static name() {
-        return 'B';
-    }
-}
-class C extends A {
-    static name() {
-        return 'C';
-    }
-}
-console.log(B.fromThis());
-console.log(C.fromThis());
+};
+
+var calculate = new Calculator()
+// console.log(calculate.evaluate('127') == 127);
+// console.log(calculate.evaluate('2 + 3') == 5);
+// console.log(calculate.evaluate('2 - 3 - 4') == -5);
+// console.log(calculate.evaluate('10 * 5 / 2') == 25);
+console.log(calculate.evaluate('2 + 3 * 4 / 3 - 6 / 3 * 3 + 8') - 8 < 0.1);
